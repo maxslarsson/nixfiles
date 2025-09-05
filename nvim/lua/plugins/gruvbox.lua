9037840a81c2539bab6a6567ac93307fa19b149d
Max Larsson <max@northwoodspace.io>
2025-09-05 02:13:16 -0400

    Set gruvbox theme

--- nvim/lua/plugins/gruvbox.lua
+++ nvim/lua/plugins/gruvbox.lua
@@ -1,8 +1,8 @@
 return {
-  "ellisonleao/gruvbox.nvim"
+  "ellisonleao/gruvbox.nvim",
   priority = 1000, -- Make sure to load this before all the other start plugins.
   config = function()
     vim.cmd.colorscheme 'gruvbox'
-  end
+  end,
 }
 
