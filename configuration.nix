f4bdc9433a39b6ef8d914bf4004a135b9392b058
Max Larsson <maxslarsson@gmail.com>
2025-09-09 13:08:28 -0400

    Update config

--- configuration.nix
+++ configuration.nix
@@ -22,15 +22,54 @@
 
   system.defaults = {
     NSGlobalDomain = {
-      InitialKeyRepeat = 10;
-      KeyRepeat = 3;
+      InitialKeyRepeat = 15;
+      KeyRepeat = 5;
       ApplePressAndHoldEnabled = false;
+
+      AppleInterfaceStyle = "Dark";
+      NSAutomaticCapitalizationEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
+    };
+    controlcenter = {
+      BatteryShowPercentage = true;
+    };
+    menuExtraClock = {
+      ShowSeconds = true;
+    };
+    screencapture = {
+      disable-shadow = true;
+      location = "${config.users.users.maxlarsson.home}/Desktop/Screenshots";
+    };
+    trackpad = {
+      Clicking = true;
+    };
+    finder = {
+      AppleShowAllExtensions = true;
+      FXDefaultSearchScope = "SCcf"; # Default to current folder
+      FXEnableExtensionChangeWarning = false;
+      FXPreferredViewStyle = "Nlsv"; # Default to list view in Finder windows
+      FXRemoveOldTrashItems = true; # Remove items in trash after 30 days
+      NewWindowTarget = "Home";
+      QuitMenuItem = true;
+      ShowPathbar = true;
+      ShowStatusBar = true;
+
+      ShowExternalHardDrivesOnDesktop = false;
+      ShowHardDrivesOnDesktop = false;
+      ShowMountedServersOnDesktop = false;
+      ShowRemovableMediaOnDesktop = false;
     };
     screensaver = {
       askForPassword = true;
       askForPasswordDelay = 0; # 0 seconds = immediately
     };
     dock = {
+      appswitcher-all-displays = true;
       orientation = "right";
       tilesize = 16;
       autohide = true;
