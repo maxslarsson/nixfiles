7dacf67f7326880813e6faaa092cb7181ce556c2
Max Larsson <maxslarsson@gmail.com>
2026-05-21 00:56:50 -0700

    Add neo-tree file explorer

---
+++ home-configurations/common/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,18 @@
+-- Neo-tree: a file system browser
+vim.pack.add {
+  { src = 'https://github.com/nvim-neo-tree/neo-tree.nvim', version = vim.version.range '*' },
+  'https://github.com/nvim-lua/plenary.nvim',
+  'https://github.com/MunifTanjim/nui.nvim',
+}
+
+vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
+
+require('neo-tree').setup {
+  filesystem = {
+    window = {
+      mappings = {
+        ['\\'] = 'close_window',
+      },
+    },
+  },
+}
