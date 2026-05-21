3a32453fe63593fd2af040a7fd732abff324eb55
Max Larsson <maxslarsson@gmail.com>
2026-05-21 01:11:20 -0700

    Delay which-key popup by 500ms

--- home-configurations/common/nvim/lua/plugins/which-key.lua
+++ home-configurations/common/nvim/lua/plugins/which-key.lua
@@ -3,7 +3,7 @@ vim.pack.add { 'https://github.com/folke/which-key.nvim' }
 
 require('which-key').setup {
   -- Delay between pressing a key and opening which-key (milliseconds)
-  delay = 0,
+  delay = 500,
   icons = {
     mappings = vim.g.have_nerd_font,
   },
